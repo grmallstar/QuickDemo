48dab25eb29ee766e67a80b5c2443800607c36df
grmallstar <zhangyilin1984@msn.com>
2018-04-11 16:00:18 +0800

    Initial commit

---
+++ src/com/zyl/Main.java
@@ -0,0 +1,9 @@
+package com.zyl;
+
+class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World!");// write your code here
+        System.out.println("Hi Hi");
+    }
+}
